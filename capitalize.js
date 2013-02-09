baa30912d2ee181daa31531e91381a4268e93d59
Colle <piercus@gmail.com>
2013-02-09 18:57:57 +0100

    Update capitalize.js

--- capitalize.js
+++ capitalize.js
@@ -1,4 +1,4 @@
-sand.define("core/String/capitalize", function() {
+sand.define("String/capitalize", function() {
   
   String.prototype.capitalize = function() {
     return (this.charAt(0).toUpperCase() + this.slice(1));
