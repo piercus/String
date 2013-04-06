7e61a98c82a0f6e8cf298ae2893929dfc6dd0474
Pierre Colle <piercus@gmail.com>
2013-04-06 18:13:53 +0200

    add indexesOf

---
+++ indexesOf.js
@@ -0,0 +1,11 @@
+sand.define("Array/indexesOf",function(){
+  Array.prototype.indexesOf = function(v){
+    var i,current = this.concat(),dec=0,indexes= [];
+    while((i = current.indexOf(v)) !== -1){
+      indexes.push(i+dec);
+      current = current.splice(i,i);
+      dec++;
+    }
+    return indexes;
+  }
+});
