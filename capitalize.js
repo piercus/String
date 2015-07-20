57e929792e6fdc792f0f096efecd4ebc43d38f8f
Colle <piercus@gmail.com>
2015-07-20 19:39:50 +0200

    to requirejs notation

--- capitalize.js
+++ capitalize.js
@@ -1,4 +1,4 @@
-sand.define("String/capitalize", function() {
+define("String/capitalize", function() {
   
   String.prototype.capitalize = function() {
     return (this.charAt(0).toUpperCase() + this.slice(1));
